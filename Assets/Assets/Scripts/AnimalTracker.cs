6b539abda7a7b1a309f249a2f08f2ae5d2982b8e
SneakyNinja2041 <135566030+SneakyNinja2041@users.noreply.github.com>
2023-12-07 12:35:41 +1100

    Fixed animal tracker and added UI for the counter
    Animal tracker now keeps track of all animals inside of the area properly, and the UI for the animals found will only be on the screen when the player is in the area

--- Assets/Assets/Scripts/AnimalTracker.cs
+++ Assets/Assets/Scripts/AnimalTracker.cs
@@ -1,28 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class AnimalTracker : MonoBehaviour
 {
-
-
-    public int animalCount;
-
+    public static int animalCount;
     public GameObject addedAnimal;
+    public TextMeshProUGUI animalCounter;
+    public GameObject animalText;
 
-    private void OnTriggerStay(Collider other)
+    void Start()
+    {
+        animalText.SetActive(false);
+    }
+
+    void Update()
+    {
+        animalCounter.text = "Animals Collected : " + animalCount.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
          if(other.gameObject.tag == "Pickup")
          {
             addedAnimal = other.gameObject;
+
             animalCount++;
+
+         }
+
+         if(other.gameObject.tag == "Player")
+         {
+            animalText.SetActive(true);
          }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        animalCount--;
+        if(other.gameObject.tag == "Pickup")
+        {
+            addedAnimal = other.gameObject;
 
+            animalCount--;
+        }
+
+        if(other.gameObject.tag == "Player")
+        {
+            animalText.SetActive(false);
+        }
 
     }
 
